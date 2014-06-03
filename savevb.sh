5405a5a83c85fb68ef23485cab734404ef270e24
maniat1k <maniat1kster@gmail.com>
2014-06-03 18:59:37 -0300

    english it up

--- savevb.sh
+++ savevb.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-# lee las vm para salvar!
 #Chequear que las maquinas estan arriba.
+#Check the running machines
 for i in `/usr/bin/VBoxManage list runningvms | grep "{\|}" | cut -d " " -f1 | tr -d "\""`;do
-        echo "Esta $i arriba."
+#list them
+        echo "VM $i is running."
+#do savestate and tells you that is done!
 	/usr/bin/VBoxManage controlvm $i savestate
-echo "Apagando"
+echo "$i is saved!"
 done
