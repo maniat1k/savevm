14ebf2f31dee4062f7af63fc20c884c50d7046ce
maniat1k <maniat1kster@gmail.com>
2014-06-04 15:07:31 -0300

    change savevb to savevm and create log

--- savevm.sh
+++ savevm.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#Chequear que las maquinas estan arriba.
 #Check the running machines
 for i in `/usr/bin/VBoxManage list runningvms | grep "{\|}" | cut -d " " -f1 | tr -d "\""`;do
 #list them
