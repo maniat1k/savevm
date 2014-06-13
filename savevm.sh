495beed2201d44a43c257218bfaccd6fa229f63b
maniat1k <maniat1kster@gmail.com>
2014-06-13 10:35:51 -0300

    final sh script

--- savevm.sh
+++ savevm.sh
@@ -2,7 +2,6 @@
 #Check the running machines
 for i in `/usr/bin/VBoxManage list runningvms | grep "{\|}" | cut -d " " -f1 | tr -d "\""`;do
 #time it. Leave two options here this is a draft.
-#export time=`date "+%H:%M"`
 export time=`date`
 #list them
         echo "Doing savestate of  $i."
