e35f605b8168e592c8f7b6f8828006d7a4830e7e
maniat1k <maniat1kster@gmail.com>
2014-10-15 13:41:18 -0200

    less lines

--- savevm.sh
+++ savevm.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
+export time=`date`
 #Check the running machines on root
 for i in `/usr/bin/VBoxManage list runningvms | grep "{\|}" | cut -d " " -f1 | tr -d "\""`;do
-#time it.
-export time=`date`
-#list them.
 echo "Doing savestate of $i at $time"
-#do savestate and save in a log file.
 /usr/bin/VBoxManage controlvm $i savestate
 echo "$i is saved! At $time" >> /tmp/savevm.log
 done
