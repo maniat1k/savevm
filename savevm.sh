fd98fe7602704250e72faa0f78cda558f3f3b258
maniat1k <maniat1kster@gmail.com>
2014-10-16 17:20:59 -0200

    auto install on hatl.local

--- savevm.sh
+++ savevm.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
+export path=`/usr/bin/realpath savevm.sh`
+export data=`/usr/bin/grep testtotal.sh /home/marcelo/test`
 export time=`date`
-#Check the running machines on root
+
+#Check if the script is saved on halt.local.
+if [[ "$data" = $(/usr/bin/realpath savevm.sh) ]]; then
+echo "loaded"
+else
+echo "missing: fixing for next time!"
+echo $path >> /etc/init.d/halt.local
+fi
+
+#Check the running machines on root.
 for i in `/usr/bin/VBoxManage list runningvms | grep "{\|}" | cut -d " " -f1 | tr -d "\""`;do
 echo "Doing savestate of $i at $time"
 /usr/bin/VBoxManage controlvm $i savestate
