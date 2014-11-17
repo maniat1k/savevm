5c7058a036a3b8fab2ad07fb4c46836a82a7105a
maniat1k <maniat1kster@gmail.com>
2014-11-17 14:01:14 -0200

    changed log path

--- savevm.sh
+++ savevm.sh
@@ -15,5 +15,5 @@ fi
 for i in `/usr/bin/VBoxManage list runningvms | grep "{\|}" | cut -d " " -f1 | tr -d "\""`;do
 echo "Doing savestate of $i at $time"
 /usr/bin/VBoxManage controlvm $i savestate
-echo "$i is saved! At $time" >> /tmp/savevm.log
+echo "$i is saved! At $time" >> /var/log/savevm.log
 done
